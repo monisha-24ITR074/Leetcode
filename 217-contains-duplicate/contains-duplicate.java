4aa1f9738131fd7b6fad8dc98a29c1b6c01b3fd4
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-04-02 21:03:24 +0530

    Time: 15 ms (65.03%) | Memory: 96.8 MB (17.54%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int num = nums[i];
+            if(map.containsKey(num)){
+                return true;
+            }
+            map.put(num, 1);
+        }
+        return false;
+    }
+}
