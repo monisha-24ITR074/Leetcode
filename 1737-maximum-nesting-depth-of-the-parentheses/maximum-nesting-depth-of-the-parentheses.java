d4f367a49a3e60e4d109a20a6e686edbf78b43c0
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-04-06 10:26:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (97.19%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxDepth(String s) {
+        int depth=0;int max=0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+
+            if(ch == '('){
+                depth++;
+                max = Math.max(max,depth);
+            }
+            else if(ch==')'){
+                depth--;
+            }
+        }
+        return max;
+    }
+}
