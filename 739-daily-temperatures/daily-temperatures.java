e932bc40523a4ace65eed284359a78b430108386
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-04-06 15:47:19 +0530

    Time: 77 ms (20.78%) | Memory: 106.2 MB (77.40%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] arr) { // int[] temperature
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack <Integer> st = new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && arr[st.peek()]<=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=0;
+            }
+            else{
+                ans[i]= st.peek()-i;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+}
