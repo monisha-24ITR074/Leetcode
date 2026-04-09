bb69cc73fdbb63188dd6d2c77993360c5b4576e6
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-04-09 14:37:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (93.08%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+                 return mid;
+            if(nums[left]<=nums[mid]){
+                if(target>=nums[left]){
+                    if(nums[mid]>target){
+                        right=right-1;
+                    }
+                    else {
+                        left=left+1;
+                    }
+                }
+                else{
+                    left=mid+1;
+                }
+            }else{
+                if(target<=nums[right]){
+                    if(nums[mid]<target){
+                        left=mid+1;
+                    }else{
+                        right=mid-1;
+                    }
+                }else{
+                    right=mid-1;
+                }
+            }
+        }
+    return -1;
+    }
+}
