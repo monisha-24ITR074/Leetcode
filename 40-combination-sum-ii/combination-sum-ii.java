a70649203375017c6d576bb105dc55ecddf220ab
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-06-02 11:00:27 +0530

    Time: 8 ms (14.55%) | Memory: 50.5 MB (5.42%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(0,candidates,target,new ArrayList<>(),ans);
+        return ans;
+    }
+    private void backtrack(int index,int[] candidates,int target,List<Integer> curr,List<List<Integer>> ans){
+        if(target==0){
+            ans.add(new ArrayList<>(curr));
+            return ;
+        }
+        if(target<0){
+            return;
+        }
+        for(int i=index;i < candidates.length;i++){
+            if(i > index && candidates[i]==candidates[i-1]){
+                continue;
+            }
+            curr.add(candidates[i]);
+            backtrack(i+1,candidates,target-candidates[i],curr,ans);
+            curr.remove(curr.size()-1);
+        }
+    }
+}
