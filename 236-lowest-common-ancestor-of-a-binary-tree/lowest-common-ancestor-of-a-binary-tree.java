10a19da7aba8e37b69c4f0b8114559a2863d5fe3
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-06-04 22:05:43 +0530

    Time: 11 ms (7.84%) | Memory: 46.8 MB (11.11%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,17 +9,88 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) {
-            return root;
+        List<TreeNode> pathp=new ArrayList<>();
+        List<TreeNode> pathq=new ArrayList<>();
+        List<TreeNode> ans1=new ArrayList<>();
+        List<TreeNode> ans2=new ArrayList<>();
+        find(root,p,ans1,pathp);
+        find(root,q,ans2,pathq);
+        TreeNode ans=null;
+
+        for (int i=0;i<Math.min(pathp.size(),pathq.size());i++){
+            if(pathp.get(i)==pathq.get(i)){
+                ans=pathp.get(i);
+            }
+            else{
+                break;
+            }
         }
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left==null){
-            return right;
+        return ans;
+    }
+    public void find(TreeNode root,TreeNode target,List<TreeNode> curr,List<TreeNode> result){
+        if(root==null){
+            return;
         }
-        if(right==null){
-            return left;
+        curr.add(root);
+        if(root==target){
+            result.addAll(curr);
+            return;
         }
-        return root;
+        find(root.left,target,curr,result);
+        find(root.right,target,curr,result);
+        curr.remove(curr.size()-1);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         List<TreeNode> pathp=new ArrayList<>();
+//         List<TreeNode> pathq=new ArrayList<>();
+//         List<TreeNode> ans1=new ArrayList<>();
+//         List<TreeNode> ans2=new ArrayList<>();
+//         find(root,p,ans1,pathp);
+//         find(root,q,ans2,pathq);
+//         TreeNode ans=null;
+
+//         for (int i=0;i<Math.min(pathp.size(),pathq.size());i++){
+//             if(pathp.get(i)==pathq.get(i)){
+//                 ans=pathp.get(i);
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+//     public void find(TreeNode root,TreeNode target,List<TreeNode> curr,List<TreeNode> result){
+//         if(root==null){
+//             return;
+//         }
+//         curr.add(root);
+//         if(root==target){
+//             result.addAll(curr);
+//             return;
+//         }
+//         find(root.left,target,curr,result);
+//         find(root.right,target,curr,result);
+//         curr.remove(curr.size()-1);
+
+//     }
+// }
