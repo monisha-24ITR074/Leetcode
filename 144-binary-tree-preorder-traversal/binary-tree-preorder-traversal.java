1ba6041fe3a05b5c62077281b523845c74329098
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-06-05 09:34:33 +0530

    Time: 1 ms (6.75%) | Memory: 43 MB (68.23%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -16,15 +16,32 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        preorder(root,ans);
+        Stack<TreeNode> st = new Stack<>();
+        if(root==null){
+            return ans;
+        }
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode s = st.pop();
+            ans.add(s.val);
+            if(s.right!=null){
+                st.push(s.right);
+            }
+            if(s.left!=null){
+                st.push(s.left);
+            }
+        }
         return ans;
     }
-    private void preorder(TreeNode root,List<Integer> ans){
-        if(root==null){
-            return;
-        }
-        ans.add(root.val);
-        preorder(root.left,ans);
-        preorder(root.right,ans);
-    }
 }
+    //     List<Integer> ans = new ArrayList<>();
+    //     preorder(root,ans);
+    //     return ans;
+    // }
+    // private void preorder(TreeNode root,List<Integer> ans){
+    //     if(root==null){
+    //         return;
+    //     }
+    //     ans.add(root.val);
+    //     preorder(root.left,ans);
+    //     preorder(root.right,ans);
