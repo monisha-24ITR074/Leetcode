1dc69240fc5ee39aa45d71064330f048ee2a016f
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-06-12 11:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (15.89%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,30 +1,13 @@
 class Solution {
-    public int fib(int n) { 
-    //     // memorization  - top down
-    //     int[] dp = new int[n+1] ;
-    //     Arrays.fill(dp,-1);
-    //     int ans = solve(n,dp);
-    //     return ans;
-    // }
-    // public int solve(int n,int[] dp){ // dp means dynamic programming
-
-    //     if(n <= 1) {
-    //         return n;
-    //     }
-    //     if(dp[n] != -1){
-    //         return dp[n];
-    //     }
-    //     dp[n]=solve(n-1,dp)+solve(n-2,dp);
-    //     return dp[n];
-
-        int[] dp = new int[n+1];  //Tabulation method - bottom-up
+    public int fib(int n) {
         if(n<=1) return n;
-        dp[0]=0 ;
-        dp[1]=1;
-
+        int prev2=0;
+        int prev1=1;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            int temp=prev2+prev1;
+            prev2 = prev1;
+            prev1 = temp;
         }
-        return dp[n];
+        return prev1;
     }
 }
