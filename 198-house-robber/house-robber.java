8f480672c734d8073cf2bb0f6decb4a340d30311
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-06-13 11:59:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (36.47%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -4,14 +4,13 @@ class Solution {
         if(n==1){
             return nums[0];
         }
-        int[] dp = new int[n];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
+        int prev2=nums[0];
+        int prev1=Math.max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
-            int rob = nums[i]+dp[i-2];
-            int notrob = 0 + dp[i-1];
-            dp[i] = Math.max(rob,notrob);
+            int rob = Math.max(nums[i]+prev2,prev1);
+            prev2=prev1;
+            prev1=rob;
         }
-        return dp[n-1];
+        return prev1;
     }
 }
