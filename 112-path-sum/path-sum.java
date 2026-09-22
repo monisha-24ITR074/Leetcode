1927b3f21b31ec032cc00860b2c591df4b424500
monisha-24ITR074 <monishan.24it@kongu.edu>
2026-09-22 22:40:57 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (14.87%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -15,11 +15,9 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null){
-            return false;
-        }
+        if(root==null) return false;
         if(root.left==null && root.right==null){
-            return targetSum==root.val;
+            return targetSum == root.val;
         }
         boolean l = hasPathSum(root.left,targetSum-root.val);
         boolean r = hasPathSum(root.right,targetSum-root.val);
